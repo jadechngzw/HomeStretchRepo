c6e8ffc5a462ba0cc8b1cd9181b959e8f5206d57
Anisha <anisha.karnik@duke.edu>
2026-03-22 22:28:04 -0400

    Converted patients page design from figma to streamlit

--- streamlit-app/clinician_app.py
+++ streamlit-app/clinician_app.py
@@ -1,11 +1,25 @@
 import streamlit as st
 
-st.title("March 20, 2026")
+st.set_page_config(
+    page_title = "Dashboard"
+)
 
-col1, col2 = st.columns(2)
+# st.title("Welcome to the Clinician App")
+# st.sidebar.success("Select a page")
 
-with col1:
-    # stuff for left side
-    st.image("examplepic.jpg", caption="Video of patient working out")
-with col2:
-    # stuff for right side
+pg = st.navigation([
+    st.Page("app_pages/Patients.py", title="Patients", icon=None),
+    st.Page("app_pages/Weekly_Progress.py", title="Weekly Progress", icon=None),
+    st.Page("app_pages/Alerts.py", title="Alerts", icon=None),
+    st.Page("app_pages/Messages.py", title="Messages", icon=None)
+])
+
+pg.run()
+
+# col1, col2 = st.columns(2)
+
+# with col1:
+#     # stuff for left side
+#     st.image("examplepic.jpg", caption="Video of patient working out")
+# # with col2:
+#     # stuff for right side
